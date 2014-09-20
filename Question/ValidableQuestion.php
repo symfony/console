3840bc4ebfab01372fc7d87fb0529c2feb31aefd
p4bloch <pchiappetti@gmail.com>
2014-09-20 14:37:41 -0300

    Added ValidableQuestion

---
+++ Question/ValidableQuestion.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Symfony\Component\Console\Question;
+
+/**
+ * Represents a Validable question.
+ *
+ */
+class ValidableQuestion extends Question{
+
+    private $rules;
+
+    public function __construct($question, array $rules, $default = null)
+    {
+        parent::__construct($question, (bool) $default);
+
+        $this->rules = $rules;
+        $this->setValidator($this->validate());
+    }
+
+    private function validate()
+    {
+        $rules = $this->rules;
+
+        return function ($answer) use ($rules) {
+            foreach ($rules as $rule){
+                $answer = $this->validateRule($rule, $answer);
+            }
+            return $answer;
+        };
+    }
+
+    private function validateRule($rule, $answer)
+    {
+        if ($rule == 'required'){
+            if (!$answer) {
+                throw new \InvalidArgumentException(
+                    'Come on, give me something!'
+                );
+            }
+        }
+
+        if ($rule == 'numeric'){
+            if (!is_numeric($answer)) {
+                throw new \RuntimeException(
+                    'Please, enter a num3r1c value'
+                );
+            }
+        }
+    }
+}
